3863014fd0764feabff0288dd51aa04906334bda
Peter Matthews <peter.matthews@fullycoded.co.uk>
2018-09-17 15:57:27 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -3,12 +3,7 @@
 iisreset;
 
 $env = Get-Content "C:\AzureData\CustomData.bin" -First 1
-
-cd 'c:\tspazuredata\'
-
-& .\sm-$env.ps1
-
 $password = ConvertTo-SecureString "$env:TSP_SM_LOCAL_PASSWORD" -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\spinnaker", $password)
 
-start-process powershell.exe -Credential $credential {cd 'c:\tspservicemanager\'; & .\setup.ps1 -integrationsetup $true;}
+start-process powershell.exe -Credential $credential {cd 'c:\tspazuredata\';& .\sm-$env.ps1;c:\tspservicemanager\';& .\setup.ps1 -integrationsetup $true;}
