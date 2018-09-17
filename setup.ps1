525ac64bd3e82d3429bb739d64a6dc36a9cfcff1
Peter Matthews <peter.matthews@fullycoded.co.uk>
2018-09-17 11:03:36 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,11 +1,13 @@
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+
 iisreset;
 
 $env = Get-Content "C:\AzureData\CustomData.bin" -First 1
 
 cd 'c:\tspazuredata\'
 
-& .\$env.bat
+& .\sm-$env.ps1
 
-refreshenv
+'c:\tspservicemanager\';
 
-start powershell {[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; cd 'c:\tspservicemanager\'; & .\setup.ps1 -integrationsetup $true; Read-Host }
+& .\setup.ps1 -integrationsetup $true;
