d25242785b7f20da2798ee1336296d86f6552bf6
Peter Matthews <peter.matthews@fullycoded.co.uk>
2018-09-18 11:13:25 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -5,12 +5,13 @@ cd 'c:\tspazuredata\'
 & .\sm-$environment.ps1
 
 [scriptblock]$block = {
+ $environment = Get-Content "C:\AzureData\CustomData.bin" -First 1
  cd 'c:\tspazuredata\'; 
  & .\sm-$environment.ps1; 
  cd 'c:\tspservicemanager\'; 
- & .\setup.ps1 -integrationsetup $true;
+ & .\setup.ps1 -integrationsetup $true; Read-Host;
 };
 
 $password = ConvertTo-SecureString "$env:VM_PASSWORD" -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\spinnaker", $password)
-start powershell $block.Invoke() -Credential $credential
+start powershell $block -Credential $credential
