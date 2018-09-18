aa34b95ebcb97b34224372d2eced1f6174916289
Peter Matthews <peter.matthews@fullycoded.co.uk>
2018-09-18 23:45:19 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,7 +1,7 @@
 C:\ProgramData\chocolatey\bin\choco.exe install -y sysinternals
 
-net start server;
-net start workstation;
+net stop server && net start server;
+net stop workstation && net start workstation;
 
 cd 'c:\tspazuredata\'; 
 & .\sm-dev.ps1;
