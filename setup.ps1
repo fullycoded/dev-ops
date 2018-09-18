6806dd0a590b3bd562c395c63560668ed78e9c81
Peter Matthews <peter.matthews@fullycoded.co.uk>
2018-09-18 11:07:45 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -8,7 +8,7 @@ cd 'c:\tspazuredata\'
  cd 'c:\tspazuredata\'; 
  & .\sm-$environment.ps1; 
  cd 'c:\tspservicemanager\'; 
- & .\setup.ps1 -integrationsetup $true; Read-Host;
+ & .\setup.ps1 -integrationsetup $true;
 };
 
 $password = ConvertTo-SecureString "$env:VM_PASSWORD" -AsPlainText -Force
