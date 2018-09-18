d62e0f0ab3af977c4c5d2c8ab01d9f860874dc25
Peter Matthews <peter.matthews@fullycoded.co.uk>
2018-09-18 20:00:26 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,10 +1,13 @@
-
-iisreset;
-
 cd 'c:\tspazuredata\'; 
 & .\sm-dev.ps1;
 
 $password = ConvertTo-SecureString "$env:VM_PASSWORD" -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\spinnaker", $password)
-$command = { cd 'c:\tspazuredata\'; & .\sm-dev.ps1; 'c:\tspservicemanager\'; & .\setup.ps1 -integrationsetup $true; }
-start-process powershell -ArgumentList "{-command invoke-command -scriptblock {$command}}" -Credential $credential -Wait
+
+$command = { cd 'c:\tspazuredata\'; & .\sm-dev.ps1; cd 'c:\tspservicemanager\'; & .\setup.ps1 -integrationsetup $true; }
+
+$tempFile = "$env:TEMP\temp-setup.ps1"
+
+"$command; Remove-Item `$PSCommandPath" | Out-file $tempFile
+
+Start-Process powershell.exe -ArgumentList @("-nologo -file `"$tempFile`"") -Wait -Credential $credential
