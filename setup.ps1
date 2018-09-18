d64c866d755cf04b82d4424018bf3b4812c5a817
Peter Matthews <peter.matthews@fullycoded.co.uk>
2018-09-18 20:29:36 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,8 +1,10 @@
+C:\ProgramData\chocolatey\bin\choco.exe install -y sysinternals
+
 cd 'c:\tspazuredata\'; 
 & .\sm-dev.ps1;
 
-$password = ConvertTo-SecureString "$env:VM_PASSWORD" -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\spinnaker", $password)
+$password = "$env:VM_PASSWORD"
+$username = "spinnaker"
 
 $command = { cd 'c:\tspazuredata\'; & .\sm-dev.ps1; cd 'c:\tspservicemanager\'; & .\setup.ps1 -integrationsetup $true; }
 
@@ -10,4 +12,4 @@ $tempFile = "$env:TEMP\temp-setup.ps1"
 
 "$command; Remove-Item `$PSCommandPath" | Out-file $tempFile
 
-Start-Process powershell.exe -ArgumentList @("-nologo -file `"$tempFile`"") -Wait -Credential $credential
+psexec -u $username -p $password powershell.exe $tempFile
