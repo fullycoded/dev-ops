391f0e17a7ea97592ad1b5076ef2a25848c5f436
Peter Matthews <peter.matthews@fullycoded.co.uk>
2018-09-18 13:16:36 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -14,4 +14,4 @@ cd 'c:\tspazuredata\'
 
 $password = ConvertTo-SecureString "$env:VM_PASSWORD" -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\spinnaker", $password)
-Start-Process powershell $block -Credential $credential -Wait
+start powershell $block -Credential $credential -Wait
