04a4ff86caadde485f549303a90df328a7c66496
Peter Matthews <peter.matthews@fullycoded.co.uk>
2018-09-18 16:04:43 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,12 +1,17 @@
-iisreset;
+iisreset
 
 $environment = Get-Content "C:\AzureData\CustomData.bin" -First 1
 cd 'c:\tspazuredata\'
 & .\sm-$environment.ps1
 
-$password = ConvertTo-SecureString "$env:VM_PASSWORD" -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\spinnaker", $password) 
+[scriptblock]$block = {
+ $environment = Get-Content "C:\AzureData\CustomData.bin" -First 1
+ cd 'c:\tspazuredata\'; 
+ & .\sm-$environment.ps1; 
+ cd 'c:\tspservicemanager\'; 
+ & .\setup.ps1 -integrationsetup $true; Read-Host;
+}
 
-Enable-PSRemoting –force
-Invoke-Command -ScriptBlock { $environment = Get-Content "C:\AzureData\CustomData.bin" -First 1; cd 'c:\tspazuredata\'; & .\sm-$environment.ps1; cd 'c:\tspservicemanager\'; & .\setup.ps1 -integrationsetup $true; } -Credential $credential -ComputerName $env:COMPUTERNAME -Wait
-Disable-PSRemoting -Force
+$password = ConvertTo-SecureString "$env:VM_PASSWORD" -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\spinnaker", $password)
+start powershell $block -Credential $credential -Wait
