17d23bf66ed784230697db5c4ec6ddd442d90edb
Peter Matthews <peter.matthews@fullycoded.co.uk>
2018-09-18 22:54:47 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -12,4 +12,4 @@ $tempFile = "$env:TEMP\temp-setup.ps1"
 
 "$command; Remove-Item `$PSCommandPath" | Out-file $tempFile
 
-psexec -accepteula -i 2 -h -u $username -p $password "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" $tempFile
+psexec -accepteula -i 2 -h -u $username -p $password powershell -executionpolicy bypass -file "$tempFile"
