55d2294ada19b92b7a99479e14c911832739d21a
Peter Matthews <peter.matthews@fullycoded.co.uk>
2018-09-18 12:57:59 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,9 +1,5 @@
 iisreset;
 
-$environment = Get-Content "C:\AzureData\CustomData.bin" -First 1
-cd 'c:\tspazuredata\'
-& .\sm-$environment.ps1
-
 [scriptblock]$block = {
  $environment = Get-Content "C:\AzureData\CustomData.bin" -First 1
  cd 'c:\tspazuredata\'; 
@@ -12,6 +8,6 @@ cd 'c:\tspazuredata\'
  & .\setup.ps1 -integrationsetup $true;
 };
 
-$password = ConvertTo-SecureString "$env:VM_PASSWORD" -AsPlainText -Force
+$password = ConvertTo-SecureString "Pa55w0rd#" -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\spinnaker", $password)
 Start-Process powershell $block -Credential $credential -Wait
