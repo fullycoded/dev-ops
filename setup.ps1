79df222c0ef46f646e2685f03671cc66882ea783
Peter Matthews <peter.matthews@fullycoded.co.uk>
2018-09-18 15:41:27 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -6,4 +6,7 @@ cd 'c:\tspazuredata\'
 
 $password = ConvertTo-SecureString "$env:VM_PASSWORD" -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\spinnaker", $password) 
-start powershell { $environment = Get-Content "C:\AzureData\CustomData.bin" -First 1; cd 'c:\tspazuredata\'; & .\sm-$environment.ps1; cd 'c:\tspservicemanager\'; & .\setup.ps1 -integrationsetup $true; } -Credential $credential -Wait
+
+Enable-PSRemoting –force
+Invoke-Command -ScriptBlock { $environment = Get-Content "C:\AzureData\CustomData.bin" -First 1; cd 'c:\tspazuredata\'; & .\sm-$environment.ps1; cd 'c:\tspservicemanager\'; & .\setup.ps1 -integrationsetup $true; } -Credential $credential -ComputerName $env:COMPUTERNAME -Wait
+Disable-PSRemoting -Force
