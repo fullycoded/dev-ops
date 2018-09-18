17e165f029ea4ad060296889327b902b12174c86
Peter Matthews <peter.matthews@fullycoded.co.uk>
2018-09-18 16:57:35 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,12 +1,4 @@
 
 iisreset;
 
-$env = Get-Content "C:\AzureData\CustomData.bin" -First 1
-
-cd 'c:\tspazuredata\'
-
-& .\sm-dev.ps1
-
-refreshenv
-
 start powershell { cd 'c:\tspazuredata\'; & .\sm-dev.ps1; cd 'c:\tspservicemanager\'; & .\setup.ps1 -integrationsetup $true; } -Wait
