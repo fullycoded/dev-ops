8bd0f5d11302bbe2d5b19c258ccefd941fd9697a
Peter Matthews <peter.matthews@fullycoded.co.uk>
2018-09-18 19:37:47 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -6,5 +6,5 @@ cd 'c:\tspazuredata\';
 
 $password = ConvertTo-SecureString "$env:VM_PASSWORD" -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\spinnaker", $password)
-
-start powershell { cd 'c:\tspazuredata\'; & .\sm-dev.ps1; cd 'c:\tspservicemanager\'; & .\setup.ps1 -integrationsetup $true; } -Credential $credential -Wait
+$command = { cd 'c:\tspazuredata\'; & .\sm-dev.ps1; c:\tspservicemanager\'; & .\setup.ps1 -integrationsetup $true; }
+start-process powershell -ArgumentList "{-command invoke-command -scriptblock {$command}}" -Credential $credential -Wait
