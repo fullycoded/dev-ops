90daea94388119aab0a7dbd4283d65c3ffa19c3e
Peter Matthews <peter.matthews@fullycoded.co.uk>
2018-09-18 18:16:41 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,7 @@
 
 iisreset;
 
-start powershell { cd 'c:\tspazuredata\'; & .\sm-dev.ps1; cd 'c:\tspservicemanager\'; & .\setup.ps1 -integrationsetup $true; } -Wait
+$password = ConvertTo-SecureString "$env:VM_PASSWORD" -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\spinnaker", $password)
+
+start powershell { cd 'c:\tspazuredata\'; & .\sm-dev.ps1; cd 'c:\tspservicemanager\'; & .\setup.ps1 -integrationsetup $true; } -Credential $credential -Wait
