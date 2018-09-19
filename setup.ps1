477f676ef981a5bb1d393ccba224f18098bcd0ca
Peter Matthews <peter.matthews@fullycoded.co.uk>
2018-09-19 10:16:00 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -3,16 +3,18 @@ C:\ProgramData\chocolatey\bin\choco.exe install -y sysinternals
 net start server;
 net start workstation;
 
+$environment = Get-Content "C:\AzureData\CustomData.bin" -First 1
+
 cd 'c:\tspazuredata\'; 
-& .\sm-dev.ps1;
+& .\sm-$environment.ps1;
 
 $password = "$env:VM_PASSWORD"
 $username = "spinnaker"
 
-$command = { cd 'c:\tspazuredata\'; & .\sm-dev.ps1; cd 'c:\tspservicemanager\'; & .\setup.ps1 -integrationsetup $true; }
+$command = { cd 'c:\tspazuredata\'; & .\sm-$environment.ps1; cd 'c:\tspservicemanager\'; & .\setup.ps1 -integrationsetup $true; }
 
-$tempFile = "c:\temp-setup.ps1"
+$tempFile = "c:\bootstrap.ps1"
 
 "$command" | Out-file $tempFile
 
-psexec -accepteula -h -u $username -p $password cmd /c "echo . | powershell -executionpolicy bypass -file c:\temp-setup.ps1"
+psexec -accepteula -h -u $username -p $password cmd /c "echo . | powershell -executionpolicy bypass -file $tempFile"
